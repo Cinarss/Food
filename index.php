706db72ed91742fbfdbd3ed47d1ae57e0ef54dcd
cinarss <sakcinar5@gmail.com>
2022-08-21 01:08:01 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/global.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+        <div class="header">
+            <div class="header-container">
+                <ul class="navbar">
+                    <a href=""><li>Home Page</li></a>
+                    <a href=""><li>Foods</li></a>
+                    <a href=""><li>Share Food</li></a>
+                    <a href=""><li>My Foods</li></a>
+                    <a href=""><li>Log In</li></a>
+                </ul>
+            </div>
+        </div>
+
+        <div class="header-image">
+            <div class="image-header-container">
+                <div class="image"><img src="image/homeFood.jpg" alt=""></div>
+            </div>
+        </div>
+
+
+</body>
+</html>
